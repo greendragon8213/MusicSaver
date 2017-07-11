8cf8c04e385fdf6bc11ce561848304ac3f619040
Tetiana Lypka <greendragon8213@gmail.com>
2017-07-12 02:17:02 +0300

    About & How it works implemented;

--- MusicDownloader/MusicDownloader/Controllers/HomeController.cs
+++ MusicDownloader/MusicDownloader/Controllers/HomeController.cs
@@ -1,4 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Mvc;
+using MusicDownloader.Models;
+using MusicDownloader.Resources;
 
 namespace MusicDownloader.Controllers
 {
@@ -17,6 +26,25 @@ namespace MusicDownloader.Controllers
 
         public ActionResult FAQ()
         {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [System.Web.Mvc.HttpPost]
+        public async Task<ActionResult> SendContactMeMessage([FromBody]ContactMeMessageVM contactMeMessage)
+        {
+            //if (songsList == null || songsList.Count == 0)
+            //    throw new ArgumentException(ErrorMessages.SongsListIsEmpty);
+
+            //string temporaryFilesPath = Path.Combine(Server.MapPath("~"),
+            //    (ConfigurationManager.AppSettings["TemporaryFilesFolderName"]));
+            //string savedFileName = await _musicArchiveService.CreateMusicArchive(songsList, temporaryFilesPath);
+
+            //return Json(new { fileName = savedFileName });
             return View();
         }
     }
---
+++ MusicDownloader/MusicDownloader/Models/ContactMeMessageVM.cs
@@ -0,0 +1,9 @@
+﻿namespace MusicDownloader.Models
+{
+    public class ContactMeMessageVM
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
