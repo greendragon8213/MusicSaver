348a7da9cfa920780d9b50adc26d8bf3c93e82d4
Tetiana <greendragon8213@gmail.com>
2017-07-29 17:06:27 +0300

    Images optimization implemented; js/css bundles added

---
+++ MusicDownloader/MusicDownloader/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Optimization;
+
+namespace MusicDownloader
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/utils-js").Include(
+                "~/Scripts/modernizr-2.6.2.js",
+                "~/Scripts/jquery-1.10.2.min.js",
+                "~/Scripts/jquery.localize.min.js",
+                "~/Scripts/bootstrap.min.js",
+                "~/Scripts/jquery.blockUI.js"
+                ));
+
+            bundles.Add(new ScriptBundle("~/bundles/app-js").Include(
+                "~/Scripts/app/constants.js",
+                "~/Scripts/app/main.js",
+                "~/Scripts/app/contact-me-form.js",
+                "~/Scripts/app/index.js"
+                ));
+
+            bundles.Add(new StyleBundle("~/bundles/app-css").Include(
+                "~/Content/app/site.css",
+                "~/Content/app/index.css",
+                "~/Content/app/how-it-works.css",
+                "~/Content/app/faq.css",
+                "~/Content/app/about.css"
+                ));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
--- MusicDownloader/MusicDownloader/Global.asax.cs
+++ MusicDownloader/MusicDownloader/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 using DI;
 using log4net;
@@ -14,6 +15,7 @@ namespace MusicDownloader
             
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
 
         }
     }
